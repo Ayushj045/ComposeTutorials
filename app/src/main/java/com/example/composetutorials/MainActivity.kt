3a7b4573911ba1be49aaa1ef1740363ba6f5f33c
Ayush <jaiswalaman231211@gmail.com>
2023-01-24 01:38:35 +0530

    JetPackCompose

--- app/src/main/java/com/example/composetutorials/MainActivity.kt
+++ app/src/main/java/com/example/composetutorials/MainActivity.kt
@@ -13,7 +13,6 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -22,74 +21,78 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.composetutorials.ui.theme.ComposeTutorialsTheme
+import org.w3c.dom.Text
+import androidx.compose.material.Text as Text1
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val fontFamily = FontFamily(
+            Font(R.font.lato_italic, FontWeight.Thin),
+            Font(R.font.lato_bold, FontWeight.Light),
+            Font(R.font.lato_black, FontWeight.Black),
+            Font(R.font.lato_bolditalic, FontWeight.ExtraBold),
+            Font(R.font.lato_blackitalic, FontWeight.Bold)
+        )
         setContent {
-           val painter = painterResource(id = R.drawable.spiderman)
-            val description = "Spiderman-Far From Home"
-            val title = "Spiderman-Far From Home"
-            Box(modifier = Modifier
-                .fillMaxWidth(0.5f)
-                .padding(16.dp)
-            ){
-                ImageCard(
-                    painter = painter,
-                    contentDescription = description,
-                    title = title
-                )
-            }
-
-        }
-    }
-}
-
-@Composable
-fun ImageCard(
-    painter: Painter,
-    contentDescription: String,
-    title: String,
-    modifier: Modifier = Modifier
-){
-    Card(
-        modifier = modifier.fillMaxWidth(),
-        shape = RoundedCornerShape(15.dp),
-        elevation = 5.dp
-    ){
-        Box(modifier = modifier.height(200.dp)){
-            Image(
-                painter = painter,
-                contentDescription = contentDescription,
-                contentScale = ContentScale.Crop
-            )
-            Box(modifier = Modifier
-                .fillMaxSize()
-                .background(
-                    Brush.verticalGradient(
-                        colors = listOf(
-                            Color.Transparent,
-                            Color.Black
-                        ),
-                        startY = 300f
-                    )
-                ))
             Box(
                 modifier = Modifier
                     .fillMaxSize()
-                    .padding(12.dp),
-            contentAlignment = Alignment.BottomStart
-            ){
-                Text(title, style = TextStyle(color = Color.White, fontSize = 16.sp))
+                    .background(Color(0xFF101010))){
+                androidx.compose.material.Text(
+                    text = buildAnnotatedString {
+                                                withStyle(
+                                                    style = SpanStyle(
+                                                        color = Color.Green,
+                                                        fontSize = 50.sp
+                                                    )
+                                                ){
+                                                    append("J")
+                                                }
+                        append("etpack ")
+                        withStyle(
+                            style = SpanStyle(
+                                color = Color.Green,
+                                fontSize = 50.sp
+                            )
+                        ){
+                            append("C")
+                        }
+                        append("ompose")
+                    } ,
+                    color = Color.White,
+                    fontSize = 30.sp,
+                    fontFamily = fontFamily,
+                    fontWeight = FontWeight.Bold,
+                    fontStyle = FontStyle.Italic,
+                    textAlign = TextAlign.Center,
+                    textDecoration = TextDecoration.Underline
+                )
             }
         }
     }
 }
+
+
+
+
+
+
+
 
 
 
